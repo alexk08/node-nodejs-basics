9db8b06f6161ed6f7853d7ac27230ee996db8be3
aleksandr.krasilnikov <alexander.krasilni@aliexpress.ru>
2023-06-22 20:27:48 +0300

    add create subtask implementation

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,52 @@
+import { open, writeFile } from "fs/promises";
+// import { open, close, write } from "fs";
+
+const path = "./files/fresh.txt";
+const innerText = "I am fresh and young";
+const errorMessage = "FS operation failed";
+
 const create = async () => {
-    // Write your code here 
+  let fileHandle;
+
+  try {
+    fileHandle = await open(path, "wx");
+    await writeFile(fileHandle, innerText);
+  } catch (err) {
+    if (err.code === "EEXIST") {
+      throw new Error(errorMessage);
+    }
+
+    throw err;
+  } finally {
+    await fileHandle?.close();
+  }
 };
 
 await create();
+
+//! Callback implementation
+// const create = () => {
+//   open(path, "wx", (err, fd) => {
+//     if (err) {
+//       if (err.code === "EEXIST") {
+//         throw new Error(errorMessage);
+//       }
+
+//       throw err;
+//     }
+
+//     try {
+//       write(fd, innerText, (err) => {
+//         if (err) {
+//           throw err;
+//         }
+//       });
+//     } finally {
+//       close(fd, (err) => {
+//         if (err) throw err;
+//       });
+//     }
+//   });
+// };
+
+// create();
