e083fd46054586c20b82f8c2315fb93e99da2fb1
aleksandr.krasilnikov <alexander.krasilni@aliexpress.ru>
2023-06-23 11:08:55 +0300

    add rename and delete implementation

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
-const remove = async () => {
-    // Write your code here 
+import { rm } from "fs/promises";
+import { getDirname } from "../helpers/utils.mjs";
+import { ENOENT, ERROR_MESSAGE } from "../helpers/constants.mjs";
+
+const dirname = getDirname(import.meta.url);
+const filePath = `${dirname}/files/fileToRemove.txt`;
+
+const remove = async (path) => {
+  try {
+    await rm(path);
+  } catch (err) {
+    if (err.code === ENOENT) {
+      throw new Error(ERROR_MESSAGE);
+    }
+    throw err;
+  }
 };
 
-await remove();
+await remove(filePath);
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,27 @@
-const rename = async () => {
-    // Write your code here 
+import fs from "fs/promises";
+import { existsSync } from "fs";
+
+import { getDirname } from "../helpers/utils.mjs";
+import { ENOENT, ERROR_MESSAGE } from "../helpers/constants.mjs";
+
+const dirname = getDirname(import.meta.url);
+
+const oldPath = `${dirname}/files/wrongFilename.txt`;
+const newPath = `${dirname}/files/properFilename.md`;
+
+const rename = async (oldPath, newPath) => {
+  try {
+    if (existsSync(newPath)) {
+      throw new Error(ERROR_MESSAGE);
+    }
+    await fs.rename(oldPath, newPath);
+  } catch (err) {
+    if (err.code === ENOENT) {
+      throw new Error(ERROR_MESSAGE);
+    }
+    throw err;
+    
+  }
 };
 
-await rename();
+await rename(oldPath, newPath);
