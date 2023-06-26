93c307264f21cb4839f531ad162da8651abb4c68
aleksandr.krasilnikov <alexander.krasilni@aliexpress.ru>
2023-06-26 13:03:12 +0300

    add list and read tasks implementation

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,26 @@
-const list = async () => {
-    // Write your code here 
+import { opendir } from "fs/promises";
+import { ENOENT, ERROR_MESSAGE } from "../helpers/constants.mjs";
+import { getDirname } from "../helpers/utils.mjs";
+
+const dirname = getDirname(import.meta.url);
+const dirPath = `${dirname}/files`;
+
+const list = async (dirPath) => {
+  try {
+    const dir = await opendir(dirPath);
+    const fileNames = [];
+
+    for await (const dirent of dir) {
+      fileNames.push(dirent.name);
+    }
+
+    console.log(fileNames);
+  } catch (err) {
+    if (err.code === ENOENT) {
+      throw new Error(ERROR_MESSAGE);
+    }
+    throw err;
+  }
 };
 
-await list();
+await list(dirPath);
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
-const read = async () => {
-    // Write your code here 
+import { readFile } from "fs/promises";
+import { ENOENT, ERROR_MESSAGE } from "../helpers/constants.mjs";
+import { getDirname } from "../helpers/utils.mjs";
+
+const dirname = getDirname(import.meta.url);
+const file = `${dirname}/files/fileToRead.txt`;
+
+const read = async (filePath) => {
+  try {
+    const content = await readFile(filePath, { encoding: "utf-8" });
+    console.log(content);
+  } catch (err) {
+    if (err.code === ENOENT) {
+      throw new Error(ERROR_MESSAGE);
+    }
+  }
 };
 
-await read();
+await read(file);
