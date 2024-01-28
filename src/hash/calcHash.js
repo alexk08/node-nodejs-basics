78ff3dd5b104f155bee208348ff66277af394bcb
Aleksandr Krasilnikov <alexander.krasilni@aliexpress.ru>
2024-01-28 13:58:15 +0300

    add hash task implementation

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import { createReadStream } from "node:fs";
+import { getDirname } from "../helpers/utils.mjs";
+
+const dirname = getDirname(import.meta.url);
+const filename = "fileToCalculateHashFor.txt";
+const filePath = `${dirname}/files/${filename}`;
+
 const calculateHash = async () => {
-    // Write your code here 
+  const { createHash } = await import("node:crypto");
+  const hash = createHash("sha256");
+
+  const input = createReadStream(filePath);
+  input.on("readable", () => {
+    const data = input.read();
+    if (data) hash.update(data);
+    else {
+      console.log(`SHA256 hash for file ${filename}: ${hash.digest("hex")}`);
+    }
+  });
 };
 
 await calculateHash();
