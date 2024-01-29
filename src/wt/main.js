9b3ca453cea341ce5020bed2045cfcc614ce0d83
Aleksandr Krasilnikov <alexander.krasilni@aliexpress.ru>
2024-01-29 23:52:48 +0300

    add worker task implentation

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,17 @@
+import { Worker } from "worker_threads";
+import { getDirname } from "../helpers/utils.mjs";
+
+const dirname = getDirname(import.meta.url);
+
+const worker = new Worker(`${dirname}/worker.js`);
+worker.postMessage(10);
+
+worker.on("message", (result) => {
+  console.log("Result of nthFibonacci(10): ", result, "\nPress Ctrl + C to exit");
+});
+
 const performCalculations = async () => {
-    // Write your code here
+  // Write your code here
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,16 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { parentPort } from "worker_threads";
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+// n should be received from main thread
+const nthFibonacci = (n) => (n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2));
+
+parentPort.on("message", (n) => {
+  const result = nthFibonacci(n);
+  sendResult(result);
+});
+
+const sendResult = (result) => {
+  // This function sends result of nthFibonacci computations to main thread
+  parentPort.postMessage(result);
 };
 
-sendResult();
+// sendResult();
