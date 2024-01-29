d9101f197258351f29ed2094e79462e679a3b461
Aleksandr Krasilnikov <alexander.krasilni@aliexpress.ru>
2024-01-29 22:11:35 +0300

    add compress task implementation

--- src/helpers/utils.mjs
+++ src/helpers/utils.mjs
@@ -1,5 +1,17 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import { rm } from "fs/promises";
+import { ENOENT, ERROR_MESSAGE } from "../helpers/constants.mjs";
 
 export const getFilename = (url) => fileURLToPath(url);
 export const getDirname = (url) => dirname(getFilename(url));
+export const removeFile = async (path) => {
+  try {
+    await rm(path);
+  } catch (err) {
+    if (err.code === ENOENT) {
+      throw new Error(ERROR_MESSAGE);
+    }
+    throw err;
+  }
+};
