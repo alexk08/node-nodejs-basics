e7d83c496807d80c6f3aa7ed6ccefbef20749404
Aleksandr Krasilnikov <alexander.krasilni@aliexpress.ru>
2024-01-29 21:22:43 +0300

    add stream tasks implementation

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import { createWriteStream } from "node:fs";
+import { getDirname } from "../helpers/utils.mjs";
+import { stdin, stdout } from "node:process";
+
+const dirname = getDirname(import.meta.url);
+const filename = "fileToWrite.txt";
+const filePath = `${dirname}/files/${filename}`;
+
 const write = async () => {
-    // Write your code here 
+  const writable = createWriteStream(filePath);
+  stdout.write(
+    "Введите текст. После этого нажмите Ctrl + C и текст сохраниться в файле files/fileToWrite.txt \n"
+  );
+  stdin.pipe(writable);
 };
 
 await write();
