cce198c08832c79f16468431bfe9a5aaa7ff7db8
Aleksandr Krasilnikov <alexander.krasilni@aliexpress.ru>
2024-01-29 22:34:06 +0300

    add decompress task implementation

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,30 @@
+import { createGunzip } from "node:zlib";
+import { pipeline } from "node:stream";
+import { createReadStream, createWriteStream } from "node:fs";
+import { getDirname, removeFile } from "../helpers/utils.mjs";
+import { promisify } from "node:util";
+
+const pipe = promisify(pipeline);
+
 const decompress = async () => {
-    // Write your code here 
+  const dirname = getDirname(import.meta.url);
+  const archive = `${dirname}/files/archive.gz`;
+  const targetFilename = "fileToCompress.txt";
+  const targetFilePath = `${dirname}/files/${targetFilename}`;
+
+  const gunzip = createGunzip();
+  const source = createReadStream(archive);
+  const destination = createWriteStream(targetFilePath);
+
+  try {
+    await pipe(source, gunzip, destination);
+    await removeFile(archive);
+  } catch (err) {
+    if (!err) return;
+
+    console.error("An error occurred:", err);
+    process.exitCode = 1;
+  }
 };
 
 await decompress();
